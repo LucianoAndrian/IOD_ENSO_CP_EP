dd957c3a0755fdbddd83d8c1d2c5c1a8cbcb96c3
LucianoAndrian <lucianoandrian@hotmail.com>
2025-08-01 13:12:15 -0300

    save txt

--- porcentaje_de_casos.py
+++ porcentaje_de_casos.py
@@ -2,6 +2,9 @@
 Comparacion de % de casos en cada categorias para OBS y CFSv2
 """
 # ---------------------------------------------------------------------------- #
+out_dir = '/home/luciano.andrian/doc/IOD_ENSO_CP_EP/salidas/'
+
+# ---------------------------------------------------------------------------- #
 from SelectEvents_obs import Compute
 from Funciones import DMI
 import os
@@ -29,10 +32,18 @@ def Porcentaje(total, dir, replace_str):
 
                 f = f.replace('.nc', '')
                 f = f.replace(replace_str, '')
+                f = f.replace('_SON', '')
+                f = f.replace('simultaneos', 'sim')
+                f = f.replace('dobles', 'sim')
+                f = f.replace('pos', 'p')
+                f = f.replace('neg', 'n')
+                f = f.replace('opuestos', 'op')
+                f = f.replace('triples', 'tr')
 
                 porcentaje[f] = per
 
         porcentaje = pd.DataFrame(porcentaje.values(), porcentaje.keys() )
+        porcentaje = porcentaje.sort_index()
         print(porcentaje)
 
         return porcentaje
@@ -54,16 +65,19 @@ variables = ['dmi', 'ep', 'cp']
 Compute(variables, ds1=dmi, ds2=ep_tk, ds3=cp_tk, out_dir=temporal_out_dir,
         prefix='OBS', save=True, thr=0.75)
 porcentaje_tk = Porcentaje(total_obs, temporal_out_dir, 'OBS_')
+porcentaje_tk.to_csv(f'{out_dir}porcentaje_tk.txt', sep='\t', index=True)
 
 # Td
 Compute(variables, ds1=dmi, ds2=ep_td, ds3=cp_td, out_dir=temporal_out_dir,
         prefix='OBS', save=True, thr=0.75)
 porcentaje_td = Porcentaje(total_obs, temporal_out_dir, 'OBS_')
+porcentaje_td.to_csv(f'{out_dir}porcentaje_td.txt', sep='\t', index=True)
 
 # n
 Compute(variables, ds1=dmi, ds2=ep_n, ds3=cp_n, out_dir=temporal_out_dir,
         prefix='OBS', save=True, thr=0.75)
 porcentaje_n = Porcentaje(total_obs, temporal_out_dir, 'OBS_')
+porcentaje_n.to_csv(f'{out_dir}porcentaje_n.txt', sep='\t', index=True)
 
 # CFSv2 ---------------------------------------------------------------------- #
 total_cfsv2 = 3744
@@ -71,14 +85,19 @@ total_cfsv2 = 3744
 # Tk
 dir_cfsv2_tk =  '/pikachu/datos/luciano.andrian/cases_dates_EP_CP/'
 porcentaje_cfsv2_tk = Porcentaje(total_cfsv2, dir_cfsv2_tk, 'CFSv2_')
+porcentaje_cfsv2_tk.to_csv(f'{out_dir}porcentaje_cfsv2_tk.txt',
+                           sep='\t', index=True)
 
 # Td
 dir_cfsv2_td =  '/pikachu/datos/luciano.andrian/cases_dates_EP_CP/aux_ep_cp_t/'
 porcentaje_cfsv2_td = Porcentaje(total_cfsv2, dir_cfsv2_td, 'CFSv2_')
-
+porcentaje_cfsv2_td.to_csv(f'{out_dir}porcentaje_cfsv2_td.txt',
+                           sep='\t', index=True)
 # n
 dir_cfsv2_n =  '/pikachu/datos/luciano.andrian/cases_dates_EP_CP/aux_ep_cp_n/'
 porcentaje_cfsv2_n = Porcentaje(total_cfsv2, dir_cfsv2_n, 'CFSv2_')
+porcentaje_cfsv2_n.to_csv(f'{out_dir}porcentaje_cfsv2_n.txt',
+                          sep='\t', index=True)
 
 # ---------------------------------------------------------------------------- #
 # ---------------------------------------------------------------------------- #
